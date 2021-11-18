05b8a0d2940a678ab3715d3460c2ca9a9b939e0a
Nikhil Agarwal <nikhilag@iitk.ac.in>
2021-11-18 19:20:06 +0530

    Populated the data for 1 more year and added AQI calculating script.

---
+++ scripts/aqi_calculator.py
@@ -0,0 +1,157 @@
+# Functions for calculating subindex for each particle
+import numpy as np
+
+def PM25_subindex(x):
+    if x <= 30:
+        return x * 50 / 30
+    elif x <= 60:
+        return 50 + (x - 30) * 50 / 30
+    elif x <= 90:
+        return 100 + (x - 60) * 100 / 30
+    elif x <= 120:
+        return 200 + (x - 90) * 100 / 30
+    elif x <= 250:
+        return 300 + (x - 120) * 100 / 130
+    elif x > 250:
+        return 400 + (x - 250) * 100 / 130
+    else:
+        return 0
+
+def PM10_subindex(x):
+    if x <= 50:
+        return x
+    elif x <= 100:
+        return x
+    elif x <= 250:
+        return 100 + (x - 100) * 100 / 150
+    elif x <= 350:
+        return 200 + (x - 250)
+    elif x <= 430:
+        return 300 + (x - 350) * 100 / 80
+    elif x > 430:
+        return 400 + (x - 430) * 100 / 80
+    else:
+        return 0
+
+def SO2_subindex(x):
+    if x <= 40:
+        return x * 50 / 40
+    elif x <= 80:
+        return 50 + (x - 40) * 50 / 40
+    elif x <= 380:
+        return 100 + (x - 80) * 100 / 300
+    elif x <= 800:
+        return 200 + (x - 380) * 100 / 420
+    elif x <= 1600:
+        return 300 + (x - 800) * 100 / 800
+    elif x > 1600:
+        return 400 + (x - 1600) * 100 / 800
+    else:
+        return 0
+
+def NOx_subindex(x):
+    if x <= 40:
+        return x * 50 / 40
+    elif x <= 80:
+        return 50 + (x - 40) * 50 / 40
+    elif x <= 180:
+        return 100 + (x - 80) * 100 / 100
+    elif x <= 280:
+        return 200 + (x - 180) * 100 / 100
+    elif x <= 400:
+        return 300 + (x - 280) * 100 / 120
+    elif x > 400:
+        return 400 + (x - 400) * 100 / 120
+    else:
+        return 0
+
+def NH3_subindex(x):
+    if x <= 200:
+        return x * 50 / 200
+    elif x <= 400:
+        return 50 + (x - 200) * 50 / 200
+    elif x <= 800:
+        return 100 + (x - 400) * 100 / 400
+    elif x <= 1200:
+        return 200 + (x - 800) * 100 / 400
+    elif x <= 1800:
+        return 300 + (x - 1200) * 100 / 600
+    elif x > 1800:
+        return 400 + (x - 1800) * 100 / 600
+    else:
+        return 0
+
+def CO_subindex(x):
+    if x <= 1:
+        return x * 50 / 1
+    elif x <= 2:
+        return 50 + (x - 1) * 50 / 1
+    elif x <= 10:
+        return 100 + (x - 2) * 100 / 8
+    elif x <= 17:
+        return 200 + (x - 10) * 100 / 7
+    elif x <= 34:
+        return 300 + (x - 17) * 100 / 17
+    elif x > 34:
+        return 400 + (x - 34) * 100 / 17
+    else:
+        return 0
+
+def O3_subindex(x):
+    if x <= 50:
+        return x * 50 / 50
+    elif x <= 100:
+        return 50 + (x - 50) * 50 / 50
+    elif x <= 168:
+        return 100 + (x - 100) * 100 / 68
+    elif x <= 208:
+        return 200 + (x - 168) * 100 / 40
+    elif x <= 748:
+        return 300 + (x - 208) * 100 / 539
+    elif x > 748:
+        return 400 + (x - 400) * 100 / 539
+    else:
+        return 0
+
+def AQI_calculate(pm25, pm10, so2, nox, nh3, co, o3):
+    pm25_subindex = PM25_subindex(pm25)
+    pm10_subindex = PM10_subindex(pm10)
+    so2_subindex = SO2_subindex(so2)
+    nox_subindex = NOx_subindex(nox)
+    nh3_subindex = NH3_subindex(nh3)
+    co_subindex = CO_subindex(co)
+    o3_subindex = O3_subindex(o3)
+    aqi = round(max(pm25_subindex, pm10_subindex, so2_subindex, nox_subindex, nh3_subindex, co_subindex, o3_subindex))
+    
+    if pm25_subindex == 0 and pm10_subindex == 0:
+        return np.NaN
+    
+    cnt = 0
+    cnt += 1 if pm25_subindex == 0 else 0
+    cnt += 1 if pm10_subindex == 0 else 0
+    cnt += 1 if so2_subindex == 0 else 0
+    cnt += 1 if nox_subindex == 0 else 0
+    cnt += 1 if nh3_subindex == 0 else 0
+    cnt += 1 if co_subindex == 0 else 0
+    cnt += 1 if o3_subindex == 0 else 0
+
+    if cnt >= 3:
+        return np.NaN
+
+    return aqi
+
+def AQI_bucket_calculate(x):
+    if x <= 50:
+        return "Good"
+    elif x <= 100:
+        return "Satisfactory"
+    elif x <= 200:
+        return "Moderate"
+    elif x <= 300:
+        return "Poor"
+    elif x <= 400:
+        return "Very Poor"
+    elif x > 400:
+        return "Severe"
+    else:
+        return np.NaN
